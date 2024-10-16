9dab864faf891e57cb12dca7e2f95971407be204
julietbailey <julietbailey@localhost>
2024-10-16 14:02:19 -0500

    studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,9 +14,10 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		double xVals = (x2 - x1) * (x2 - x1);
+		double yVals = (y2 - y1) * (y2 - y1);
+		double distance = Math.sqrt(xVals + yVals);
 		return distance;
 	}
 
@@ -34,37 +35,41 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+StdDraw.setPenColor(0,109,219);
+StdDraw.filledCircle(x, y, (3.0)/(4.0)*radius);
 
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+StdDraw.setPenColor(146, 0, 0);
+StdDraw.filledCircle(x, y, (1.0)/(2.0)*radius);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+StdDraw.setPenColor(255,255,109);
+StdDraw.filledCircle(x, y, (1.0)/(4.0)*radius);
 		
 	}
 
-	/**
-	 * Return a new String which is the original source String with all occurrences
-	 * of the target character substituted by the replacement String.
-	 * 
-	 * @param source      the source String
-	 * @param target      the target character to be replaced
-	 * @param replacement the replacement String
-	 * 
-	 * @return the String which results from substituting all of the target
-	 *         characters in the source String with the replacement String
-	 */
-	public static String substituteAll(String source, char target, String replacement) {
-		String result = "";
-		// TODO: Finish this method
-		
-		return result;
-	}
+//	/**
+//	 * Return a new String which is the original source String with all occurrences
+//	 * of the target character substituted by the replacement String.
+//	 * 
+//	 * @param source      the source String
+//	 * @param target      the target character to be replaced
+//	 * @param replacement the replacement String
+//	 * 
+//	 * @return the String which results from substituting all of the target
+//	 *         characters in the source String with the replacement String
+//	 */
+//	public static String substituteAll(String source, char target, String replacement) {
+//		String result = "";
+//		// TODO: Finish this method
+//		
+//		return result;
+//	}
 
 	/**
 	 * Compute the sum of elements in an array
@@ -75,7 +80,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for(int i = 0; i < values.length; i++) {
+			sum+= values[i];
+		}
 		return sum;
 	}
 
@@ -87,16 +94,19 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
-		
-
+		for(int i = 0; i < values.length; i++) {
+			values [i] = value;
+		}
 		return values;
 	}
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
 
-	
+	public static double arrayMean(int[] values) {
+		double sum = arraySum(values);
+		return sum / values.length;
+	}
 }
